24739afae00d917d1035e01d28f6b60623eae4a7
JTSnow19 <jacobtmartin7@gmail.com>
2024-03-27 11:45:28 -0700

    shapes
    ( ) [] [=]

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //Console.WriteLine("Hello Learning05 World!");
+
+        var shapes = new List<Shape>
+        {
+            new Square("Yellow", 19.7),
+            new Rectangle("Red", 8, 12),
+            new Circle("Purple", 11.11)
+        };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
